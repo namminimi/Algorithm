3ab2efcd9cd2b9372133e290ad2bd9ffe1ef796a
namminimi <111848336+namminimi@users.noreply.github.com>
2024-03-20 13:53:16 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,12 @@
+function solution(brown, yellow) {
+    var answer = [];
+    for (var y = 3; y <= (brown+yellow)/y; y++) {
+        var x = Math.floor((brown+yellow)/y);
+        if( (x-2)*(y-2)=== yellow) {
+            answer = [x, y]
+            break;
+        }
+    }
+
+    return answer;
+}

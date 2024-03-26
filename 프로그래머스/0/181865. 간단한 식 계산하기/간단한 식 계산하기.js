857023d8e87d752eb0b5afc248e76e9625ee4c95
namminimi <111848336+namminimi@users.noreply.github.com>
2024-03-27 03:00:24 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -1,5 +1,13 @@
 function solution(binomial) {
     var answer = 0;
-    answer = eval(binomial)
+    //answer = eval(binomial)
+    const calculation = {
+        '+': (a,b) => a + b,
+        '-': (a,b) => a-b,
+        '*': (a,b) => a*b,
+    }
+    const split_num = binomial.split(' ')
+    const [a, op , b] = split_num
+    answer = calculation[op](+a, +b)
     return answer;
 }

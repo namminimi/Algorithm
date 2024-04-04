3ad3f84c7c465d5bc6d8f5f08e0a8e020f76f147
namminimi <111848336+namminimi@users.noreply.github.com>
2024-04-05 02:18:11 +0900

    [level 0] Title: 세 개의 구분자, Time: 28.10 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -4,5 +4,6 @@ function solution(myStr) {
     const new_myStr = myStr.replace(regex,',').split(',')
     const search_str = new_myStr.filter(str => str !== '')
     answer = search_str.length > 0 ? search_str : ["EMPTY"]
+    //console.log(myStr.match(/[^a-c]+/g))
     return answer;
 }

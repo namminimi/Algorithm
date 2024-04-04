1f18ca6c9db48923d6288f5b8c5eb86b362348ae
namminimi <111848336+namminimi@users.noreply.github.com>
2024-04-05 02:08:47 +0900

    [level 0] Title: 세 개의 구분자, Time: 28.06 ms, Memory: 60.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -0,0 +1,8 @@
+function solution(myStr) {
+    var answer = [];
+    const regex = /a|b|c/g
+    const new_myStr = myStr.replace(regex,',').split(',')
+    const search_str = new_myStr.filter(str => str !== '')
+    answer = search_str.length > 0 ? search_str : ["EMPTY"]
+    return answer;
+}

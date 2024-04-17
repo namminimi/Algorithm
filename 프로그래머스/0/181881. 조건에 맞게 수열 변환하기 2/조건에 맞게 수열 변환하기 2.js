0ee871e5d7f7e2e8ddfc6433c72f3878737b22e8
namminimi <111848336+namminimi@users.noreply.github.com>
2024-04-18 00:44:42 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 3.01 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -0,0 +1,35 @@
+function solution(arr) {
+    var answer = 0;
+    let current = arr
+    
+    function transform(nums) {
+        return nums.map(item => {
+            if(item >= 50 && item %2 === 0) {
+                return item / 2;
+            } else if (item < 50 && item % 2 !== 0) {
+                return item * 2 + 1;
+            } else {
+                return item
+            }
+        })
+    }
+    
+    function arraysEqual(cur, nex) {
+        if(cur.length !== nex.length) return false;
+        for(let i = 0; i < cur.length; i++) {
+            if(cur[i] !== nex[i]) return false;
+        }
+        return true
+    }
+    
+    while (true) {
+        const next = transform(current);
+        answer++;
+         console.log(current)
+        if(arraysEqual(current, next)) {
+            return answer -1 ;
+        }
+        current = next
+    }
+   
+}

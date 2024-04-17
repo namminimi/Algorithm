9dd0414daec96b81d3c52b54d2cf558b5831d120
namminimi <111848336+namminimi@users.noreply.github.com>
2024-04-18 00:13:05 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1238.15 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,6 +1,8 @@
 function solution(elements) {
     var answer = 0;
-    const extendsElements = elements.concat(elements)
+    // [7,9,1,1,4] 선형 배열로 봤을 때는 4 다음에 7인지 모르고 그림으로 봤을 때 알 수 있기 때문에 
+    //concat 을 사용해서 반복되는걸 표현해주기 위해서 elements배열을 2배로 확장함
+    const extendsElements = elements.concat(elements) 
     const nums = new Set();
     for(let i = 0; i < elements.length; i++) {
         for(let j=1; j <= elements.length; j++) {

14dc488327569549ec73f30526846274d5f4921f
namminimi <111848336+namminimi@users.noreply.github.com>
2024-04-18 01:07:23 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 5.93 ms, Memory: 34.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.js
@@ -0,0 +1,14 @@
+function solution(board, k) {
+    var answer = 0;
+    for(let i = 0; i < board.length; i++) {
+        for(let j = 0; j < board[i].length; j++) {
+            if(i+j <= k) {
+                console.log('i', i , 'j', j)
+                answer += board[i][j]
+            } else {
+                break;
+            }
+        }
+    }
+    return answer;
+}
